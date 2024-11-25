27bf8d795b9496a0ce91ff65a084705da4711cef
DanielCodes <173450189+DanielCommits@users.noreply.github.com>
2024-11-26 03:31:43 +1300

    navbar fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AdminDashboard from "./components/AdminDashboard";
-import ArticleDetail from "./components/ArticleDetail";  // New import
+import ArticleDetail from "./components/ArticleDetail";  
+import Navbar from "./components/Navbar";  
 
 function App() {
   return (
     <Router>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/article/:id" element={<ArticleDetail />} /> {/* Add this route */}
+        <Route path="/article/:id" element={<ArticleDetail />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
