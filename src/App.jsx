6ad8166c328cbae858c22c9cdb5e4a627781096c
DanCodesss <173450189+DanielCommits@users.noreply.github.com>
2024-11-25 16:54:26 -0800

    navbar again bro

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-///import AdminDashboard from "./components/AdminDashboard";
-import Admin from './components/Admin';
-import ArticleDetail from "./components/ArticleDetail";  
-import Navbar from "./components/Navbar";  
+import ArticleDetail from "./components/ArticleDetail";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/admin" element={<AdminDashboard />} /> */}
-        <Route path="/admin" element={<Admin />} />
         <Route path="/article/:id" element={<ArticleDetail />} />
       </Routes>
     </Router>
@@ -20,4 +16,3 @@ function App() {
 }
 
 export default App;
-
