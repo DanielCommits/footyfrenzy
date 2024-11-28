050e0276fc9ca2ef3c5f0d619c5f9459c7000a46
DanCodesss <173450189+DanielCommits@users.noreply.github.com>
2024-11-27 17:53:58 -0800

    update firebase

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'footyfrenzy',
+  location: 'us-central1'
+};
+
