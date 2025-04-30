344bacd6721029fc5b9084ee3a49bfbb86e43aef
DanielCommits <173450189+DanielCommits@users.noreply.github.com>
2025-04-30 21:55:01 +0100

    fix

--- src/components/LiveMatch.jsx
+++ src/components/LiveMatch.jsx
@@ -4,7 +4,7 @@ import "./LiveMatch.css";
 const matches = [
   {
     competition: "Champions League",
-    status: "90'",
+    status: "FT",
     homeTeam: {
       abbreviation: "BAR",
       logo: "https://ssl.gstatic.com/onebox/media/sports/logos/paYnEE8hcrP96neHRNofhQ_96x96.png",
