5488075e70287caba7d847639390d34ac9bb9877
DanielCommits <173450189+DanielCommits@users.noreply.github.com>
2025-06-04 19:25:53 +0100

    added nice loading shi to app

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
       manifest: {
-        name: 'FootyFrenzy',
-        short_name: 'FootyFrenzy',
-        description: 'Your average amazing app. Now installable!',
-        theme_color: '#000000',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
+        name: "FootyFrenzy",
+        short_name: "FootyFrenzy",
+        description: "Your average amazing app. Now installable!",
+        theme_color: "#23272f", // modern dark grey
+        background_color: "#23272f", // modern dark grey
+        display: "standalone",
+        start_url: "/",
         icons: [
           {
-            src: '/WhatsApp Image 2025-06-04 at 18.45.01.jpeg.192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/WhatsApp Image 2025-06-04 at 18.45.01.jpeg.192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/CompressJPEG.Online_img(512x512).jpg',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            src: "/CompressJPEG.Online_img(512x512).jpg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
