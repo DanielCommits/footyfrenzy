dc025bbae85eeb83da6244eaeb7271cb7101f65e
DanielCommits <173450189+DanielCommits@users.noreply.github.com>
2025-06-13 15:33:29 +0100

    changed ig social link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -68,7 +68,7 @@ function Footer() {
           </li>
           <li>
             <a
-              href="https://www.instagram.com/thatoneguyyx/"
+              href="https://www.instagram.com/d4nrick/"
               target="_blank"
               rel="noopener noreferrer"
             >
